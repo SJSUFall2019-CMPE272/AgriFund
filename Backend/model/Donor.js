da249f0503ffded5047ea8f09490d9c342e23810
Nivali Reddy <nivalireddy.sripati@sjsu.edu>
2019-11-28 18:34:55 -0800

    Minor changes

--- Backend/model/Donor.js
+++ Backend/model/Donor.js
@@ -4,37 +4,29 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 mongoose.connect(
-    process.env.DB_CONNECT_ISSUE,
+    process.env.DB_CONNECT_DONOR,
     { useNewUrlParser: true },
     () => console.log('Connected to the database!')
 );
 
-const issueSchema = new mongoose.Schema({
-    issueName: {
+const donorSchema = new mongoose.Schema({
+    issueId: {
         type: String,
         required: true
     },
-    description: {
+    donorName: {
         type: String,
         required: true
     },
-    farmer_name: {
+    donatedAmount: {
         type: String,
         required: true
     },
-    issue_created_date: {
+    donatedDate: {
         type: Date,
         required: true,
         default: Date.now
-    },
-    requested_amount: {
-        type: String,
-        required: true
-    },
-    raised_amount: {
-        type: String,
-        required: true
     }
 });
 
-module.exports = mongoose.model('Issue', issueSchema);
+module.exports = mongoose.model('Donor', donorSchema);
