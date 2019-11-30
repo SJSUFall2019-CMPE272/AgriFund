5ef514731869b56e0f6f9186473122a83eb4dd59
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2019-11-30 12:49:11 -0800

    updated donor dashboard

--- Frontend/src/app/donor-dashboard/donor-dashboard.component.ts
+++ Frontend/src/app/donor-dashboard/donor-dashboard.component.ts
@@ -1,21 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from '../services/main.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
 
-export interface PeriodicElement1 {
-  name: string;
-  position: number;
-  startDate: any;
-  status:any;
-}
 
-const ELEMENT_DATA: PeriodicElement1[] = [
-  {position: 1, name: 'Crops',startDate: "08/07/2019", status:'Open'},
-  {
-    position: 2, name: 'Tools',startDate: "08/07/2019", status:'Closed'
-  }
- 
- 
-];
 @Component({
   selector: 'app-donor-dashboard',
   templateUrl: './donor-dashboard.component.html',
@@ -24,21 +13,64 @@ const ELEMENT_DATA: PeriodicElement1[] = [
 export class DonorDashboardComponent implements OnInit {
 
 
-
+  ELEMENT_DATA:any[]=[
+    {
+        Key: "ISSUE2",
+        Record: {
+            "description": "crops description updated again",
+            "docType": "farmer_issue",
+            "donators": [],
+            "farmer_name": "ramya",
+            "issue": "crops",
+            "issue_created_date": "11-27-2019",
+            "problem_faced": "problem faced updated again",
+            "raised_amount": "0",
+            "requested_amount": "30000",
+            "solution_proposed": "solution updated again",
+            "status": "closed"
+        }
+    },
+    {
+        Key: "ISSUE9",
+        Record: {
+            "description": "need money to get a borewell",
+            "docType": "farmer_issue",
+            "donators": [],
+            "farmer_name": "ramya",
+            "issue": "water problem",
+            "issue_created_date": "11-28-2019",
+            "problem_faced": "problem faced to be updated",
+            "raised_amount": 0,
+            "requested_amount": 5000,
+            "solution_proposed": "solution to be updated",
+            "status": "open"
+        }
+    }
+  ]
   username:any
-  displayedColumns: string[] = ['Id', 'Task Name','Due At','Status'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['Id', 'Task Name','Due At'];
+  dataSource = new MatTableDataSource<any>(this.ELEMENT_DATA);
  issuesforSPecificUser:any
-  constructor(private _Mainservice: MainService) { }
-getAllIssuesForSpecificUsersResponce:any
+  constructor(private _Mainservice: MainService,private http : HttpClient,private router:Router) { }
+
   ngOnInit() {
+    this.username=sessionStorage.getItem('name')
+   this.getAllDonationsForUser()
     if(sessionStorage.getItem('flag')==='true'){
       sessionStorage.setItem('flag','false')
       location.reload()
     }
   }
-  getMyissues(){
-    //this.issuesforSPecificUser=this._Mainservice.getAllIssueForUser(this.username)
+  getAllDonationsForUser(){
+    let header = new HttpHeaders();
+    header.append('Content-Type', 'application/json');
+     this.http.get("https://backend-agrifund.mybluemix.net/donors/"+this.username,{headers: header}).subscribe((res) => {
+      this.ELEMENT_DATA=<any>res
+      this.dataSource=new MatTableDataSource<any>(this.ELEMENT_DATA)
+            console.log(res);
+            return res
+        });
+        
   }
 
 }
